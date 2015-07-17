b86e13a0ac2fc86fddd5a1ffa977899abc6813cd
RobertoAlicata <RobertoAlicata@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-17 16:05:01 +0000

    TinyMCE button added
    git-svn-id: https://plugins.svn.wordpress.org/ra-qrcode/trunk@1201091 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ra-qrcode.php
+++ ra-qrcode.php
@@ -2,7 +2,7 @@
 /* 
 Plugin Name: ra_qrcode
 Plugin URI: http://www.robertoalicata.it/ra_qrcode
-Version: 1.0.0
+Version: 1.1.0
 Author: Roberto Alicata
 Author URI: http://www.robertoalicata.it
 Description: a simple WordPress plugin to generate a QR code with a shortcode
@@ -62,5 +62,41 @@ function ra_qrcode_shortcode( $atts ) {
 
 
 add_shortcode( 'qrcode', 'ra_qrcode_shortcode' );
+
+
+add_action('admin_head', 'ra_add_tc_button');
+
+function ra_add_tc_button() {
+    global $typenow;
+    // check user permissions
+    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
+    return;
+    }
+    // verify the post type
+    if( ! in_array( $typenow, array( 'post', 'page' ) ) )
+        return;
+    // check if WYSIWYG is enabled
+    if ( get_user_option('rich_editing') == 'true') {
+	
+        add_filter("mce_external_plugins", "ra_add_tinymce_plugin");
+        add_filter('mce_buttons', 'ra_register_my_tc_button');
+    }
+}
+
+function ra_add_tinymce_plugin($plugin_array) {
+    $plugin_array['ra_qrcode_button'] = plugins_url( '/js/qrcode-button.js', __FILE__ ); // CHANGE THE BUTTON SCRIPT HERE
+    return $plugin_array;
+}
+
+function ra_register_my_tc_button($buttons) {
+   array_push($buttons, "ra_qrcode_button");
+   return $buttons;
+}
+
+function ra_code_css() {
+    wp_enqueue_style('ra-code-css', plugins_url('/css/style.css', __FILE__));
+}
+ 
+add_action('admin_enqueue_scripts', 'ra_code_css');
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
-﻿=== ra_qrcode ===
+=== ra_qrcode ===
 Contributors: RobertoAlicata
 Tags: qr code, qrcode, qr code generator, qrcode generator, qr code shortcode
 Donate link: http://www.robertoalicata.it
 Requires at least: 3.0
 Tested up to: 4.2
-Stable tag: 1.0.0
+Stable tag: 1.1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -15,18 +15,12 @@ Usage:
 write [qrcode] to generate a QRcode for the actual url with size of 100x100 pixels
 
 you can specify these attributes:
-
 size 	(it indicates the size in pixels for width and height, default: 100)
-
 alt  	(it indicates the alternative text for the image: default "scan QR code")
-
 content (leave it blank to pass the actual url or write the content to encode)
-
 click 	(write "yes" to make the image clickable)
 
-
 example: [qrcode size=200 content="www.robertoalicata.it" alt="scan me NOW" click="yes"]
-
 
 == Installation ==
 1. Upload the plugin directory to the /wp-content/plugins/ directory
@@ -36,12 +30,13 @@ example: [qrcode size=200 content="www.robertoalicata.it" alt="scan me NOW" clic
 .
 
 == Screenshots ==
-1. the simplest shortcode
-
-2. the qr code generated
+1. The simplest shortcode
+2. The qr code generated
 
 == Changelog ==
-1.0.0 First stable version
+1.1.0 NEW feature: a tinyMCE button for configure the qrcode shortcut
+
+1.0.0 Fisrt stable version
 
 0.1.0 First version of the plugin
 
